9959137180bd7db6db70d619ad3f5ffd76ba859b
itchir <itchirfatma@gmai.com>
2023-08-22 15:45:52 +0200

    pokemonCard

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,14 @@
+
+
+function PokemonCard () {
+    return(
+        <figure className= "pokemon-card">
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="" />
+            <figcaption>bulbasaur</figcaption>
+        </figure>
+    );
+
+}
+   
+export default PokemonCard;
+
